417400e171280f3b42fb56df324f3424d0bb7348
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-03-28 18:42:43 -0500

    Estructura Back Front

---
+++ back/myapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=100, unique=True),
+    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
+class product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    product_description = models.TextField()
+    product_brand = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(upload_to='products/')
+    product_category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.product_name
+    
+class category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.category_name
